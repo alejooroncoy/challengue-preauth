f92d2b64d11f8bc07bc2d83c8f0171c543656f8c
alejooroncoy <adoa2705@gmail.com>
2025-04-02 11:42:56 -0500

    feat: Game-1 finished success

---
+++ game-1/__tests__/game1.test.ts
@@ -0,0 +1,63 @@
+import { expect, test, describe } from "vitest";
+import getFirstSubsetSumN from "../game1";
+
+describe("Test función getFirstSubsetSumN", () => {
+
+  const testCases = [
+    {
+      input: [1, 2, 3, 4, 5],
+      target: 6,
+      expected: [1, 5],
+      description: "Números enteros positivos, n es positivo (suma existe)"
+    },
+    {
+      input: [-1, -2, -3, -4, -5],
+      target: -6,
+      expected: [-1, -5],
+      description: "Números enteros negativos, n es negativo (suma existe)"
+    },
+    {
+      input: [-1, 2, -3, 4, -5],
+      target: 1,
+      expected: [-1, 2],
+      description: "Números enteros mixtos, n es positivo (suma existe)"
+    },
+    {
+      input: [-1, 2, -3, 4, -5],
+      target: -8,
+      expected: [-3, -5],
+      description: "Números enteros mixtos, n es negativo (suma existe)"
+    },
+    {
+      input: [1, 2, 3, 4, 5],
+      target: 10,
+      expected: null,
+      description: "Números enteros positivos, n es positivo (suma no existe)"
+    },
+    {
+      input: [-1, -2, -3, -4, -5],
+      target: -10,
+      expected: null,
+      description: "Números enteros negativos, n es negativo (suma no existe)"
+    },
+    {
+      input: [-1, 2, -3, 4, -5],
+      target: 10,
+      expected: null,
+      description: "Números enteros mixtos, n es positivo (suma no existe)"
+    },
+    {
+      input: [-1, 2, -3, 4, -5],
+      target: -10,
+      expected: null,
+      description: "Números enteros mixtos, n es negativo (suma no existe)"
+    }
+  ];
+
+  testCases.forEach(({ input, target, expected, description }) => {
+    test(description, () => {
+      expect(getFirstSubsetSumN(input, target)).toEqual(expected);
+    });
+  });
+
+});
